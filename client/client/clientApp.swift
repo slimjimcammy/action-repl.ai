9ccde607d48fcab20bff2a37ead35adcbebc364f
Cameron Yuen <yuen.cameron@gmail.com>
2023-12-20 21:48:05 -0500

    added Swift client code

---
+++ client/client/clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by Cameron Yuen on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
