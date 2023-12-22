6a93ca197ee4b530f2adfec82f43560775763cd2
Cameron Yuen <yuen.cameron@gmail.com>
2023-12-22 00:50:14 -0500

    added sample Flask app to test CI/CD pipeline

---
+++ backend/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def test_flask():
+    return "auto deploy works!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
