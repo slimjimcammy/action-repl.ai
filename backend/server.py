247bf04b03c0ed7208959e0854c44a81549a32cf
Cameron Yuen <yuen.cameron@gmail.com>
2023-12-22 17:36:05 -0500

    restructured server.py to be a lambda function instead of flask. rewrote main.yml to now auto deploy the lambda

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def test_flask():
-    return "auto deploy works! yay! again! woooo"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+def handler(event, context):
+    return {
+        "statusCode": 200, "body": "Hello, world!"
+    }
