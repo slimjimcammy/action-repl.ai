4e45b013240f298060dd2944623f2f8f923f9bb6
Cameron Yuen <yuen.cameron@gmail.com>
2023-12-22 01:52:51 -0500

    wooo

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def test_flask():
-    return "auto deploy works! yay! again!"
+    return "auto deploy works! yay! again! woooo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
