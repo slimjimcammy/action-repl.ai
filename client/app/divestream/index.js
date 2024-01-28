d25b0611167ba34a947e88d39d7d44a7f8e5984f
Cameron <yuen.cameron@gmail.com>
2024-01-28 17:34:50 -0500

    last RN commit

--- client/app/divestream/index.js
+++ client/app/divestream/index.js
@@ -1,12 +1,24 @@
 import { useState, useEffect, useRef } from "react";
-import { StyleSheet, View, SafeAreaView, Text, Button } from "react-native";
+import {
+    StyleSheet,
+    View,
+    SafeAreaView,
+    Text,
+    Button,
+    Dimensions,
+    ScrollView,
+} from "react-native";
 import { Camera } from "expo-camera";
 import { cameraWithTensors } from "@tensorflow/tfjs-react-native";
+import * as tf from "@tensorflow/tfjs";
 import { Video } from "expo-av";
+import { Image } from "expo-image";
+import jpeg from "jpeg-js";
 import * as MediaLibrary from "expo-media-library";
 import * as ScreenOrientation from "expo-screen-orientation";
+import * as FileSystem from "expo-file-system";
 
-const tensorCamera = cameraWithTensors(Camera);
+const TensorCamera = cameraWithTensors(Camera);
 
 function Divestream() {
     const [cameraPermitted, setCameraPermission] = useState(null);
@@ -14,6 +26,8 @@ function Divestream() {
     // const [libPermitted, setLibPermitted] = useState(null);
     const [isRecording, setIsRecording] = useState(false);
     const [videoData, setVideoData] = useState();
+    const [frames, setFrames] = useState([]);
+    const [currentFrameIndex, setCurrentFrameIndex] = useState(0);
     const cameraRef = useRef(null);
 
     useEffect(() => {
@@ -22,6 +36,7 @@ function Divestream() {
             const micStatus = await Camera.requestMicrophonePermissionsAsync();
             setCameraPermission(cameraStatus);
             setMicPermission(micStatus);
+            await tf.ready();
         })();
     }, []);
 
@@ -31,48 +46,114 @@ function Divestream() {
         return <Text>Camera not permitted for usage.</Text>;
     }
 
-    const recordVideo = () => {
-        setIsRecording(true);
-        const options = {
-            quality: "1080p",
-            maxDuration: "60",
-            mute: false,
+    const handleCameraStream = (tensors) => {
+        const loop = async () => {
+            const nextTensor = tensors.next().value;
+
+            const [height, width] = nextTensor.shape;
+            const data = new Buffer(
+                tf
+                    .concat(
+                        [nextTensor, tf.ones([height, width, 1]).mul(255)],
+                        [-1]
+                    )
+                    .slice([0], [height, width, 4])
+                    .dataSync()
+            );
+
+            const rawImageData = { data, width, height };
+            const jpegImageData = jpeg.encode(rawImageData, 200);
+
+            const imgBase64 = tf.util.decodeString(
+                jpegImageData.data,
+                "base64"
+            );
+            const salt = `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
+            const uri =
+                FileSystem.documentDirectory +
+                `tensor-${salt}-${currentFrameIndex}.jpg`;
+            await FileSystem.writeAsStringAsync(uri, imgBase64, {
+                encoding: FileSystem.EncodingType.Base64,
+            });
+
+            setFrames((prevFrames) => [...prevFrames, uri]);
+
+            requestAnimationFrame(loop);
         };
-        cameraRef.current.recordAsync(options).then((video) => {
-            console.log(video);
-            setVideoData(video);
-            setIsRecording(false);
-        });
+
+        loop();
     };
 
-    const stopRecording = () => {
-        setIsRecording(false);
-        cameraRef.current.stopRecording();
+    const handleScroll = (event) => {
+        const offsetX = event.nativeEvent.contentOffset.x;
+        const newIndex = Math.floor(offsetX / 100); // Assuming each frame is 100 pixels wide
+        setCurrentFrameIndex(newIndex);
     };
 
-    if (videoData) {
-        return (
-            <SafeAreaView style={styles.container}>
-                <Video
-                    style={styles.video}
-                    source={{ uri: videoData.uri }}
-                    useNativeControls
-                    resizeMode="contain"
-                    isLooping
-                />
-            </SafeAreaView>
-        );
-    }
+    const textureDims =
+        Platform.OS === "ios"
+            ? {
+                  width: Dimensions.get("window").width,
+                  height: Dimensions.get("window").height,
+              }
+            : {
+                  width: Dimensions.get("window").width,
+                  height: Dimensions.get("window").height,
+              };
+    const tensorDims = {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    };
 
     return (
-        <Camera style={styles.container} ref={cameraRef}>
-            <View style={styles.buttonContainer}>
-                <Button
-                    title={!isRecording ? "Start Recording" : "End Recording"}
-                    onPress={!isRecording ? recordVideo : stopRecording}
+        <>
+            {frames ? (
+                <TensorCamera
+                    ref={cameraRef}
+                    style={{
+                        // flex: 0,
+                        width: "75%",
+                        height: "100%",
+                    }}
+                    zoom={0}
+                    onReady={(tensors) => {
+                        handleCameraStream(tensors);
+                    }}
+                    type={Camera.Constants.Type.front}
+                    cameraTextureHeight={200}
+                    cameraTextureWidth={200}
+                    resizeHeight={200}
+                    resizeWidth={200}
+                    resizeDepth={3}
+                    autorender={true}
+                    rotation={90}
                 />
-            </View>
-        </Camera>
+            ) : (
+                <ScrollView
+                    horizontal
+                    pagingEnabled
+                    onScroll={handleScroll}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    {frames.map((frame, index) => (
+                        <Image
+                            key={index}
+                            source={{ uri: frame.uri }}
+                            style={{ width: 100, height: 100 }}
+                        />
+                    ))}
+                </ScrollView>
+            )}
+        </>
+
+        // <Camera style={styles.container} ref={cameraRef}>
+        //     <View style={styles.buttonContainer}>
+        //         <Button
+        //             title={!isRecording ? "Start Recording" : "End Recording"}
+        //             onPress={!isRecording ? recordVideo : stopRecording}
+        //         />
+        //     </View>
+        // </Camera>
     );
 }
 
